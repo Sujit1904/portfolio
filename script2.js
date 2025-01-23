c097d1efd624214c275db04d27a8c96a3a3e00fc
Sujit1904 <103412794+Sujit1904@users.noreply.github.com>
2025-01-23 23:45:01 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,20 @@
+const nav_t = document.querySelector('.nav-toogle');
+const list = document.querySelector('.nav_list');
+const links = document.querySelector('.nav_link');
+
+nav_t.addEventListener('click',() =>{
+    list.classList.toggle('show');
+    nav_t.querySelector('i').classList.toggle('fa-circle-xmark');
+    nav_t.querySelector('i').classList.toggle('fa-bars');
+})
+links.forEach(link => {
+    link.addEventListener('click',function(){
+        if(window.innerWidth <= 768){
+            list.classList.toggle('show');
+            nav_t.querySelector('i').classList.toggle('fa-circle-xmark');
+            nav_t.querySelector('i').classList.toggle('fa-bars');
+        }
+
+    })
+    
+});
